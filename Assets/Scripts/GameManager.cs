c0242dc0212d82c411dbd35b48ad28b6e96b45b7
Atsuro <situniv.a26@gmail.com>
2024-07-09 20:15:47 +0900

    add : pause screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace DotShooting
 {
     public class GameManager : SingletonMonoBehaviour<GameManager>
     {
+        [SerializeField] GameObject _pausePanel;
+        [SerializeField] Button _restartBtn;
+        [SerializeField] Button _exitBtn;
+
+        bool _isPause = false;
+
+
+        public void GameQuit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
 
         public void GameOver()
         {
@@ -14,14 +30,30 @@ namespace DotShooting
             SceneManager.LoadScene("GameOver");
         }
         
+        private void PauseGame()
+        {
+            _pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        private void ResumeGame()
+        {
+            _pausePanel.SetActive(false);
+            Time.timeScale = 1;
+        }
+
         void Start()
         {
-
+            _restartBtn.onClick.AddListener(() => ResumeGame());
+            _exitBtn.onClick.AddListener(() => GameQuit());
         }
 
         void Update()
         {
-            
+            if(Input.GetKey(KeyCode.Escape) && !_isPause )
+                PauseGame();
+            if(Input.GetKey(KeyCode.Escape) && _isPause )
+                ResumeGame();
         }
     }
 }
