0b877050eccf50644b05d48c644a3472eb37fe7b
Atsuro <situniv.a26@gmail.com>
2024-07-17 17:53:58 +0900

    add : high score

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -10,8 +10,11 @@ namespace DotShooting
     public class GameOver : SingletonMonoBehaviour<GameOver>
     {
         [SerializeField] TextMeshProUGUI _playerScore;
+        [SerializeField] TextMeshProUGUI _playerHighScore;
         [SerializeField] Button _restartBtn;
         [SerializeField] Button _exitBtn;
+
+        long _highScore = 0;
 
         private void GameQuit()
         {
@@ -22,10 +25,21 @@ namespace DotShooting
 #endif
         }
 
+        private void UpdateHighScore(long score)
+        {
+            if (score > _highScore)
+            {
+                _highScore = score;
+                _playerHighScore.text = _highScore.ToString() + "   NEW!!!";
+            }
+        }
+
+
         void Start()
         {
             long score = PlayerControl._score;
             _playerScore.text = score.ToString();
+            UpdateHighScore(score);
             _restartBtn.onClick.AddListener(() => SceneManager.LoadScene("PlayScene"));
             _exitBtn.onClick.AddListener(() => GameQuit());
         }
